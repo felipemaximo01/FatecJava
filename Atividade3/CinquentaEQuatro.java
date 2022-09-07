3b3acd63dc87f17b07effa54a950ef31b4b5ceab
Felipe Maximo <102770157+felipemaximo01@users.noreply.github.com>
2022-09-07 11:13:32 -0300

    Update CinquentaEQuatro.java

--- Atividade3/CinquentaEQuatro.java
+++ Atividade3/CinquentaEQuatro.java
@@ -1,7 +1,5 @@
 package Java.Atividade3;
 
-//Felipe Maximo Colen
-//2171392211043
 /*Criar um vetor A do tipo inteiro de 5 elementos–	Solicitar os valores para o usuário e inserir no vetor A–	
 Criar um vetor B do tipo inteiro  –	Ler o vetor A e para cada elemento lido calcular o fatorial e gravar no B–	
 Após de todos os elementos do vetor A, mostrar os dois vetores */
